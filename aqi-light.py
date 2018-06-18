ed6e717444869a1ec854690e5ded1f3c0966dc96
Siequnu <11821606+Siequnu@users.noreply.github.com>
2018-06-18 21:46:55 +0800

    Turn light on when command runs

--- aqi-light.py
+++ aqi-light.py
@@ -2,35 +2,36 @@ import requests
 import json
 import subprocess
 
-def send_command (hue, saturation):
+def change_colour (hue, saturation):
 	cmd = """curl -X PUT --header "Content-Type:Application/json" --header "authorization: 092-94-999" http://192.168.31.95:51826/characteristics --data '
 {"characteristics":[{"aid":38,"iid":12,"value":""" + str(hue) + """},{"aid":38,"iid":13,"value":""" + str(saturation) + """ }]}'"""
 	process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
-api_token = '8a26809e31dc3953bacdff5795091a22f52960dd'
+def turn_on_light ():
+	cmd = """curl -X PUT --header "Content-Type:Application/json" --header "authorization: 092-94-999" http://192.168.31.95:51826/characteristics --data '{"characteristics":[{"aid":38,"iid":10,"value":true,"status":0}]}'"""
+	process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)	
 
+api_token = '8a26809e31dc3953bacdff5795091a22f52960dd'
 request = "http://api.waqi.info/search/?keyword=beijing&token=" + api_token
 
 r = requests.get(request)
-
 json_data = r.json()
 
 beijing_us_embassy_aqi = None
-
 for station in json_data['data']:
 	if (station['station']['url']) == 'beijing/us-embassy':
 		
 		beijing_us_embassy_aqi = int(station['aqi'])
 
+turn_on_light ()
 if beijing_us_embassy_aqi != None:
-	if beijing_us_embassy_aqi < 50:
-		# Turn light green
-		send_command (132, 100)
-	elif beijing_us_embassy_aqi < 125:
-		send_command (32, 100)
-	elif beijing_us_embassy_aqi < 220:
-		send_command (353, 100)
+	if beijing_us_embassy_aqi < 100:
+		change_colour (132, 100) # Turn light green
+	elif beijing_us_embassy_aqi < 150:
+		change_colour (32, 100) # Orange
+	elif beijing_us_embassy_aqi < 200:
+		change_colour (353, 100) # Red
 	else:
-		send_command (283, 100)
+		change_colour (283, 100) # Purple
 
 
